4d661e9b4c12feab4e7c4271ad3a34a21d1ad543
holbreno <43557087+holbreno@users.noreply.github.com>
2020-06-28 21:35:40 -0300

    Add files via upload

---
+++ ini.php
@@ -0,0 +1,9 @@
+<?php
+  include_once("index.html");
+  include_once("menu1.html");
+  include_once("menu2.html");
+  include_once("menu3.html");
+  include_once("menu4.html");
+  include_once("form.html");
+
+?>
